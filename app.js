41a197f7f2d3136a4e121d777127b6436c0e7750
raghuvararora <raghuvararora@gmail.com>
2018-06-29 00:50:47 +0530

    installed node and refractored stuffs

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
 
-app.use(express.static("views"));
+app.use(express.static(__dirname+"/views"));
+app.use(express.static("styles"));
+app.use(express.static("scripts"));
+app.use(express.static("images"));
 
 
 
 //app.set("view engine", "ejs");
 
 
-app.get("/", function(req, res){
-	res.sendFile(__dirname+"/views/home.html");
+app.use("/", function(req, res){
+	res.sendFile(__dirname+"/views/index.html");
 });
 
 
-app.listen(3000, console.log("app satarted"));
+app.listen(3000, console.log("app started"));
