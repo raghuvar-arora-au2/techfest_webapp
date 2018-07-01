60da1ec7949058c695e1310f164c91279b54a004
raghuvararora <raghuvararora@gmail.com>
2018-07-01 19:44:52 +0530

    connected database and implemented user model

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
+var mongoose=require("mongoose");
+
+
+mongoose.connect("mongodb://raghuvar:qwert123@ds243418.mlab.com:43418/sample");
 
 
 app.set("view engine", "ejs");
@@ -9,7 +13,17 @@ app.set("view engine", "ejs");
 app.use(express.static("styles"));
 app.use(express.static("scripts"));
 app.use(express.static("images"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+
+const eventSchema= new mongoose.Schema({
+	"name": String,
+	"image": String,
+	"info": String
+})
+
+const Event=mongoose.model("Event", eventSchema);
 
 
 app.get("/events", function(req, res){
@@ -26,7 +40,8 @@ app.get("/", function(req, res){
 });
 
 app.post("/events", function(req, res){
-
+	console.log(req.body.name, req.body.image);
+	res.redirect("/events/new")
 })
 
 app.get("/adminlogin", function(req, res){
